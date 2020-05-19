de059b49e9b38eee91aa50301c3e454333ccee77
Pattycloud <patriciakollie@yahoo.com>
2020-05-19 09:15:49 -0500

    updated terraform script

--- terraform.tf
+++ terraform.tf
@@ -2,20 +2,12 @@
 # VARIABLES
 ##################################################################################
 
-variable "aws_access_key" {
-  default = "No key"
-}
-variable "aws_secret_key" {
-  default= "No key"
-}
 variable "private_key_path" {
 default = "/home/patcloud/north-virginia.pem"
 }
+
 variable "key_name" {
 default = "north-virginia"
-}
-variable "region" {
-  default = "us-east-1"
 }
 
 variable "ami" {
@@ -30,11 +22,7 @@ variable "instancetype" {
 # PROVIDERS
 ##################################################################################
 
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
-}
+provider "aws" {}
 
 
 ##################################################################################
@@ -78,6 +66,12 @@ resource "aws_security_group" "Terraform" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
